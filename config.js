2a1a9509e7142f58787036d61a6d1a2823c0167b
liushenge01-source <liushenge01@gmail.com>
2026-09-23 19:52:42 +0800

    Connect classroom submissions to Supabase

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Supabase project URL and publishable (anon) key go here after the project is created.
 // Never put a service_role key in this file.
 window.CLASSROOM_CONFIG = {
-  url: '',
-  anonKey: ''
+  url: 'https://hseyjsqvdkmuwbtqmswz.supabase.co',
+  anonKey: 'sb_publishable_tVwEe6Q0kA93-o2OO-3rHQ_ngClRBiC'
 };
